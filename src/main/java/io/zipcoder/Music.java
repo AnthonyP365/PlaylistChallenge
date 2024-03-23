a9e4a08411c80032c62464357dc3b88be3937413
Anthony <145401343+AnthonyP365@users.noreply.github.com>
2024-03-23 18:06:10 -0400

    Finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,41 @@
 package io.zipcoder;
 
 public class Music {
-
     private String[] playList;
+
+/// CONSTRUCTOR ///
 
     public Music(String[] playList){
         this.playList = playList;
     }
 
+/// METHODS ///
+
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int k = startIndex;
+        String q = selection;
+        int forwardClicks = 0;
+        int backwardClicks = 0;
+        int result = 0;
+
+        while (!playList[k].equals(q)) {
+            k = (k + 1) % playList.length;
+            forwardClicks++;
+        }
+
+        k = startIndex;
+        while (!playList[k].equals(q)) {
+            k = (k - 1 + playList.length) % playList.length;
+            backwardClicks++;
+        }
+
+        if (forwardClicks > backwardClicks) {
+            result = backwardClicks;
+        } else if (forwardClicks < backwardClicks) {
+            result = forwardClicks;
+        } else {
+            result = forwardClicks;
+        }
+        return result;
     }
 }
